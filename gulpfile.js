3eaf8331c6da5725c794b7ce781b75697101e02d
shrue348 <shure348@gmail.com>
2016-10-18 12:19:24 +0300

    gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -104,3 +104,31 @@ gulp.task('build', [// Общий таск
     'fonts:build',
     'image:build'
 ]);
+
+gulp.task('watch', function(){
+    watch([path.watch.html], function(event, cb) {
+        gulp.start('html:build');
+    });
+    watch([path.watch.style], function(event, cb) {
+        gulp.start('style:build');
+    });
+    watch([path.watch.js], function(event, cb) {
+        gulp.start('js:build');
+    });
+    watch([path.watch.img], function(event, cb) {
+        gulp.start('image:build');
+    });
+    watch([path.watch.fonts], function(event, cb) {
+        gulp.start('fonts:build');
+    });
+});
+
+gulp.task('webserver', function () {
+    browserSync(config);
+});
+
+gulp.task('clean', function (cb) {
+    rimraf(path.clean, cb);
+});
+
+gulp.task('default', ['build', 'webserver', 'watch']);
